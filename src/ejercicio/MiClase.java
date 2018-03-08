c3b80978dc4474a586a9c0b38f9da8c092d095ff
Diego Estevez <destevez@viewnext.com>
2018-03-08 17:32:48 +0100

    Resolver conflicto

--- src/ejercicio/MiClase.java
+++ src/ejercicio/MiClase.java
@@ -3,7 +3,8 @@ package ejercicio;
 public class MiClase {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, Â¿Que tal?");
+		System.out.println("Hola, ¿Que tal?");
+		System.out.println("Me has escuchado?");
 		System.out.println("Muy bien");
 	}
 
